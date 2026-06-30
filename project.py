7ddc8b3496e6754dc46f6a91631f9abc86bd5381
nine48nine-ctrl <nine48nine@gmail.com>
2026-06-30 15:20:38 +0700

    commit

---
+++ project.py
@@ -0,0 +1,8 @@
+print ("hello world")
+print("information network technology")
+print("you are welcome to join us")
+
+print("i am hungry")
+print("he ask me to go to the restaurant")
+print("i will do \"my best\" to help you")
+print("/\\")
